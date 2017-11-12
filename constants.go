b0d16f31554b362d5e502756ed083e2a1ff77ec2
João Silva <joaosilva@revtut.net>
2017-11-12 23:31:32 +0100

    Created constants file. Added base code for the permissions system.

---
+++ constants.go
@@ -0,0 +1,19 @@
+package go_pex
+
+const (
+	// ActionWrite is used when the action is writing
+	ActionWrite = 0
+	// ActionRead is used when the action is reading
+	ActionRead = 1
+)
+
+const (
+	// PermissionNone means it hasn't any permission
+	PermissionNone = 0
+	// PermissionRead means it has reading permissions
+	PermissionRead = 1
+	// PermissionWrite means it has write permissions
+	PermissionWrite = 2
+	// PermissionReadWrite means it has read and write permissions
+	PermissionReadWrite = 3
+)
---
+++ tmp.go
@@ -0,0 +1,27 @@
+package go_pex
+
+import (
+	"reflect"
+	"encoding/json"
+)
+
+// CleanStruct removes the fields that the user can't read / write
+func CleanStruct(obj interface{}, userType uint, action uint) {
+	// Remove fields without permission to change
+	tmp := RemoveFields(obj, userType, action)
+	p := reflect.ValueOf(obj).Elem()
+	p.Set(reflect.Zero(p.Type()))
+	InterfaceToStructure(tmp, &obj)
+}
+
+// InterfaceToStructure is a method that converts a map string to value into a
+// interface
+func InterfaceToStructure(data interface{}, object interface{}) error {
+	result, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(result, &object)
+
+	return nil
+}
